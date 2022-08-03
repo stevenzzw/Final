7378821c48f3128e4de50a6765ace23be852b076
zhiwen zhao <82531844+stevenzzw@users.noreply.github.com>
2022-08-03 16:26:47 -0700

    Update final.js

--- final.js
+++ final.js
@@ -87,26 +87,29 @@ const todos = document.querySelector(".content-item");
 const generateData = () => {
     getData("http://localhost:3000/todos").then(json => {
         const item = json.map(item => {
-            if (!item.completed)
+            if (!item.completed) {
+                const{id,completed,title}=item
+                console.log(id,completed,title)
                 return `
-                <div class="item item-completed">
-                <h1 id=h1num${item.id}  onclick="handleCompleted(${item.id},${item.completed})">${item.title}</h1>
+                <div class="item item-uncompleted">
+                <h1 id=h1num${item.id}  onclick="handleCompleted(${id},${completed},${title})">${item.title}</h1>
                 <input type="text" id=inputNum${item.id}  class="inputunseen"/>
-                <div>
-                <button class="editItem" onclick="editItem(${item.id},${item.completed})">Edit</button>
-                <button class="delItem" onclick="delItem(${item.id})">Delete</button>
+                <div class="btn-group">
+                <button class="btn btn-editItem" onclick="editItem(${item.id},${item.completed})"><svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="EditIcon" aria-label="fontSize small"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg></button>
+                <button class="btn btn-delItem" onclick="delItem(${item.id})"><svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DeleteIcon" aria-label="fontSize small"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg></button>
                 </div>
                 </div>
                 
-            `
+            `}
             else {
+                
                 return `
-                <div class="item item-uncompleted">
-                <h1 id=h1num${item.id}  onclick="handleCompleted(${item.id},${item.completed})">${item.title}</h1>
+                <div class="item">
+                <h1 id=h1num${item.id} class="item-completed"  onclick="handleCompleted(${item.id},${item.completed})">${item.title}</h1>
                 <input type="text" id=inputNum${item.id}  class="inputunseen"/>
-                <div>
-                <button class="editItem" onclick="editItem(${item.id},${item.completed})">Edit</button>
-                <button class="delItem" onclick="delItem(${item.id})">Delete</button>
+                <div class="btn-group">
+                <button class="btn btn-editItem" onclick="editItem(${item.id},${item.completed})"><svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="EditIcon" aria-label="fontSize small"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg></button>
+                <button class="btn btn-delItem" onclick="delItem(${item.id})"><svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DeleteIcon" aria-label="fontSize small"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg></button>
                 </div>
                 </div>
                 
@@ -162,19 +165,19 @@ function editItem(id, completed) {
     }
 }
 
- function handleCompleted(id,completed) {
-//     let inputId = ["h1Num", id].join("")
-//     let updateItem = document.getElementById(inputId)
-//    console.log(updateItem)
-//     let newUrl = [url, id].join("/")
-    
-//     let newDate = {
-//         id: id,
-//         title: title,
-//         completed: !completed
-//     }
-//     console.log(title, completed)
-//     //updateData(newUrl, newDate)
+function handleCompleted(id, c, d) {
+    //     let inputId = ["h1Num", id].join("")
+    //     let updateItem = document.getElementById(inputId)
+    //    console.log(updateItem)
+    //     let newUrl = [url, id].join("/")
 
-console.log("Can not complete this function")
- }
+    //     let newDate = {
+    //         id: id,
+    //         title: title,
+    //         completed: !completed
+    //     }
+    //     console.log(title, completed)
+    //     //updateData(newUrl, newDate)
+
+    console.log(id, c, d)
+}
